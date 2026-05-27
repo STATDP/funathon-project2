0bf52d4f4a83cab0a77479d71a956e7d0da62950
STATDP <dpi@dst.dk>
2026-05-27 10:55:42 +0000

    q3.1 done

--- nace.py
+++ nace.py
@@ -1,6 +1,7 @@
 import mlflow
 from dotenv import load_dotenv
 import polars as pl
+from sklearn.model_selection import train_test_split
 
 load_dotenv(override=True)
 
@@ -8,8 +9,22 @@ df = pl.read_parquet("https://minio.lab.sspcloud.fr/projet-formation/diffusion/f
 ldf = str(len(df))
 n_classes = str(df['code'].n_unique())
 
-print(df.head(10))
-print("Antallet af rækker i filen: " + ldf)
-print("Antallet af forskellige NACE kode i filen: " + n_classes)
+#for row in df.iter_rows(named=True):
+#    print(row['code'])
+#    print(row['name'])
+#    print(row['label'])
+
+#print(df.head(10))
+#print("Antallet af rækker i filen: " + ldf)
+#print("Antallet af forskellige NACE kode i filen: " + n_classes)
+
+train_df, tmp_df = train_test_split(df, test_size=0.30, random_state=42, stratify=df['code'])
+val_df, test_df  = train_test_split(tmp_df, test_size=0.50, random_state=42, stratify=tmp_df['code'])
+
+X_train, y_train = train_df["label"].to_numpy(), train_df["code"].to_numpy()
+X_val, y_val = val_df["label"].to_numpy(), val_df["code"].to_numpy()
+X_test, y_test = test_df["label"].to_numpy(), test_df["code"].to_numpy()
+
+print(f"Train: {len(train_df)} | Val: {len(val_df)} | Test: {len(test_df)}")
 
 print("Done")
