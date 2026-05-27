2b0b6ed980c2141cd5ec972ad2cba149c2200935
STATDP <dpi@dst.dk>
2026-05-27 09:52:48 +0000

    q2 done

--- nace.py
+++ nace.py
@@ -1,6 +1,15 @@
 import mlflow
 from dotenv import load_dotenv
+import polars as pl
 
 load_dotenv(override=True)
+
+df = pl.read_parquet("https://minio.lab.sspcloud.fr/projet-formation/diffusion/funathon/2026/project2/generation_None_temp08.parquet")
+ldf = str(len(df))
+n_classes = str(df['code'].n_unique())
+
+print(df.head(10))
+print("Antallet af rækker i filen: " + ldf)
+print("Antallet af forskellige NACE kode i filen: " + n_classes)
 
 print("Done")
