a1b85c76b4c1dd739a86373774b56c488967bb5b
STATDP <dpi@dst.dk>
2026-05-27 11:21:30 +0000

    q4 done

--- nace.py
+++ nace.py
@@ -4,26 +4,31 @@ import polars as pl
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from torchTextClassifiers.value_encoder import ValueEncoder
-
-
+from torchTextClassifiers.tokenizers import WordPieceTokenizer
 
 load_dotenv(override=True)
 
-df = pl.read_parquet("https://minio.lab.sspcloud.fr/projet-formation/diffusion/funathon/2026/project2/generation_None_temp08.parquet")
+df = pl.read_parquet(
+    "https://minio.lab.sspcloud.fr/projet-formation/diffusion/funathon/2026/project2/generation_None_temp08.parquet"
+)
 ldf = str(len(df))
-n_classes = str(df['code'].n_unique())
+n_classes = str(df["code"].n_unique())
 
-#for row in df.iter_rows(named=True):
+# for row in df.iter_rows(named=True):
 #    print(row['code'])
 #    print(row['name'])
 #    print(row['label'])
 
-#print(df.head(10))
-#print("Antallet af rækker i filen: " + ldf)
-#print("Antallet af forskellige NACE kode i filen: " + n_classes)
+# print(df.head(10))
+# print("Antallet af rækker i filen: " + ldf)
+# print("Antallet af forskellige NACE kode i filen: " + n_classes)
 
-train_df, tmp_df = train_test_split(df, test_size=0.30, random_state=42, stratify=df['code'])
-val_df, test_df  = train_test_split(tmp_df, test_size=0.50, random_state=42, stratify=tmp_df['code'])
+train_df, tmp_df = train_test_split(
+    df, test_size=0.30, random_state=42, stratify=df["code"]
+)
+val_df, test_df = train_test_split(
+    tmp_df, test_size=0.50, random_state=42, stratify=tmp_df["code"]
+)
 
 X_train, y_train = train_df["label"].to_numpy(), train_df["code"].to_numpy()
 X_val, y_val = val_df["label"].to_numpy(), val_df["code"].to_numpy()
@@ -32,10 +37,10 @@ X_test, y_test = test_df["label"].to_numpy(), test_df["code"].to_numpy()
 print(f"Train: {len(train_df)} | Val: {len(val_df)} | Test: {len(test_df)}")
 
 encoder = LabelEncoder()
-encoder.fit(train_df['code'].to_numpy())
+encoder.fit(train_df["code"].to_numpy())
 
-all_codes  = set(df['code'])
-train_codes = set(train_df['code'])
+all_codes = set(df["code"])
+train_codes = set(train_df["code"])
 missing = all_codes - train_codes
 
 if missing:
@@ -53,4 +58,22 @@ print(f"Mindste antal observationer for en code i train: {min_count}")
 
 value_encoder = ValueEncoder(label_encoder=encoder)
 
+tokenizer = WordPieceTokenizer(vocab_size=5000, output_dim=10)
+tokenizer.train(X_train)
+
+print("Output tensor size:", tokenizer.tokenize(X_train[0]).input_ids.shape)
+print("Vocabulary size:", tokenizer.vocab_size)
+
+# Look at an example of tokenization
+print("Raw text", X_train[0])
+print(
+    "Tokens id:",
+    tokenizer.tokenize(X_train[0]).input_ids.squeeze(0)
+)
+print(
+    "Tokens:",
+    tokenizer.tokenizer.convert_ids_to_tokens(
+        tokenizer.tokenize(X_train[0]).input_ids.squeeze(0)
+    )
+)
 print("Done")
