ffbaf96972bb7f632c3c92a7f54c3991639981ee
STATDP <dpi@dst.dk>
2026-05-28 08:32:28 +0000

    q7.2 done

--- nace.py
+++ nace.py
@@ -6,6 +6,12 @@ from sklearn.preprocessing import LabelEncoder
 from torchTextClassifiers.value_encoder import ValueEncoder
 from torchTextClassifiers.tokenizers import WordPieceTokenizer
 from torchTextClassifiers import ModelConfig, TrainingConfig, torchTextClassifiers
+import s3fs
+import random
+from torchTextClassifiers.utilities.plot_explainability import (
+    map_attributions_to_char, map_attributions_to_word,
+    plot_attributions_at_char, plot_attributions_at_word, figshow,
+)
 
 load_dotenv(override=True)
 
@@ -91,7 +97,7 @@ ttc = torchTextClassifiers(
 )
 
 training_config = TrainingConfig(
-    num_epochs=5,
+    num_epochs=1,
     batch_size=128,
     lr=5 * 1e-4,
     patience_early_stopping=5,
@@ -115,5 +121,59 @@ with mlflow.start_run() as run:
         training_config.save_path,   # local folder produced by ttc.train()
         artifact_path="model_artifacts",
     )
+
+fs = s3fs.S3FileSystem(
+    anon=True,  # public bucket
+    endpoint_url="https://minio.lab.sspcloud.fr",
+)
+
+local_dir = "./mlflow-artifacts/"
+fs.get(
+    "projet-funathon/diffusion/mlflow-artifacts/",
+    local_dir,
+    recursive=True,
+)
+# Rebuild the torchTextClassifiers object from the downloaded files
+ttc = torchTextClassifiers.load(local_dir)
+
+ttc.pytorch_model.eval()
+
+random_indices = random.sample(range(len(X_test)), 3)
+example_texts = X_test[random_indices]
+example_true_codes = y_test[random_indices]
+print(example_texts)
+top_k = 5
+results = ttc.predict(example_texts, top_k=top_k, explain_with_captum=True)
+for i, text in enumerate(example_texts):
+    predicted_codes = [results["prediction"][i][k] for k in range(top_k)]
+    confidence = [results["confidence"][i][k].item() for k in range(top_k)]
+    print(f"\nText: {text}")
+    print(f"  True code: {example_true_codes[i]}")
+    for code, conf in zip(predicted_codes, confidence):
+        print(f"  {code}  (confidence: {conf:.1f})")
+
+text_idx = 0
+top_k_idx = 0
+text_sample = example_texts[text_idx]
+offsets = results["offset_mapping"][text_idx]
+word_ids = results["word_ids"][text_idx]
+predicted_code = results["prediction"][text_idx][top_k_idx]
+
+attributions = results["captum_attributions"][text_idx][top_k_idx] # (seq_len,)
+
+words, word_attributions = map_attributions_to_word(
+    attributions.unsqueeze(0), text_sample, word_ids, offsets
+)
+char_attributions = map_attributions_to_char(attributions.unsqueeze(0), offsets, text_sample)
+
+titles = [f"Attributions for NACE code {predicted_code}"]
+
+figshow(plot_attributions_at_char(
+    text=text_sample, attributions_per_char=char_attributions, titles=titles,
+)[0])
+
+figshow(plot_attributions_at_word(
+    text=text_sample, words=words.values(), attributions_per_word=word_attributions, titles=titles,
+)[0])
 
 print("Done")
